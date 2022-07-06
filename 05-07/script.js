d034dc1b6b2dd3c42cabea10b04916825da8786f
marco94gug <marco.guglielmino94@gmail.com>
2022-07-06 15:25:30 +0200

    feat: upgraded with lesson 06-07

--- 05-07/script.js
+++ 05-07/script.js
@@ -21,11 +21,11 @@ const product = {
   
 const getProductHTML = (product) => {
     
-    const {name, price, img} = product;
+    const {name, price, img, id} = product;
     
     return `<li><img src="${img}"><p class="title_product">${name}</p>
     <p class="price_product">${price}€</p>
-    <button class="buy">Compra</button></li>`;
+    <button id="${id}" class="buy">Compra</button></li>`;
   };
   
 
@@ -55,6 +55,10 @@ const getProductHTML = (product) => {
       return paginatedProducts;
     },
   
+    get getPrice() {
+      return this._product.price[id]
+    }, 
+
     set products(newProducts) {
       /**
        * Il consiglio è quello di spostare la parte di renderHTML dentro una funzione indipendente,
@@ -106,21 +110,28 @@ const getProductHTML = (product) => {
 // const button2 = document.querySelector('.second_page');
 // const button3 = document.querySelector('.third_page');
 
-let button = document.querySelectorAll('.btn_pages');
+const $buttonPages = document.querySelector('.buttons');
 
-button.forEach((button) => {
-    button.addEventListener('click', () => {
-        // let index = id;
-        // const buttonId = document.getElementById(index);
-        shop.page = Number(button.getAttribute('id'));
-        
-        // button.classList.add('active');
-        // buttonId.classList.remove('active');
-        
-    })
+// button.forEach((button) => {
+//     button.addEventListener('click', () => shop.page = Number(button.getAttribute('id')))
+// });
+
+$buttonPages.addEventListener("click", (event) => {
+  if (event.target.tagName === "BUTTON") {
+    const $button = event.target;
+    const newPage = Number($button.id);
+    shop.page = newPage;
+  };
 })
 
+const $productsContainer = document.querySelector('.products');
 
+$productsContainer.addEventListener("click", (event) => {
+  if (event.target.tagName === "BUTTON") {
+    const $idButton = event.target.id;
+    alert(`Super prezzo di oggi: ${shop.products[$idButton].price}€, Occhio! ne sono rimasti solamente ${shop.products[$idButton].stock}`);
+  }
+});
 
 // button1.addEventListener('click', () => {
 //     shop.page = button1.getAttribute('id');
@@ -146,111 +157,127 @@ shop.products = [
         id: 0,
         name: "Apple Iphone 8",
         price: 800,
-        img: "https://images.eprice.it/nobrand/0/hres/221/204024221/DAM204024221-0-8112ba83-b128-4adb-810a-6525974f9e7b.jpg"
+        img: "https://images.eprice.it/nobrand/0/hres/221/204024221/DAM204024221-0-8112ba83-b128-4adb-810a-6525974f9e7b.jpg",
+        stock: 40,
 },
 {
     id: 1,
     name: "SamsungS8",
     price: 700,
-    img: "https://www.telaccomodo.it/wp-content/uploads/2017/01/galaxy-s8.jpg"
+    img: "https://www.telaccomodo.it/wp-content/uploads/2017/01/galaxy-s8.jpg",
+    stock: 35,
+},
+{
+    id: 2,
+    name: "Patate",
+    price: 6,
+    img: "https://dimages2.gazzettaobjects.it/files/image_768_434/uploads/2022/03/01/621e19369cdb7.jpeg",
+    stock: 2,
 },
 {
     id: 3,
-    name: "Patate",
-    price: 6,
-    img: "https://dimages2.gazzettaobjects.it/files/image_768_434/uploads/2022/03/01/621e19369cdb7.jpeg"
-},
-{
-    id: 4,
     name: "Tv Samsung 55",
     price: 899,
     img: "https://i.ebayimg.com/images/g/b7UAAOSwhFdgZW7s/s-l640.jpg"
 
 },
 {
-id: 5,
+id: 4,
 name: "Sony Xperia Mini",
 price: 129,
-img: "https://m.media-amazon.com/images/I/41gUqyDwgWL._AC_.jpg"
+img: "https://m.media-amazon.com/images/I/41gUqyDwgWL._AC_.jpg",
+stock: 76
 },
 {
-id: 6,
+id: 5,
 name: "Sony Xperia P",
 price: 189,
-img: "https://www.hwupgrade.it/immagini/Sony-Xperia-P.jpg"
+img: "https://www.hwupgrade.it/immagini/Sony-Xperia-P.jpg",
+stock: 2,
 },
 {
-    id: 7,
+    id: 6,
     name: "Huawei Patate",
     price: 299,
-    img: "https://img.fruugo.com/product/5/03/113140035_max.jpg"
+    img: "https://img.fruugo.com/product/5/03/113140035_max.jpg",
+    stock: 23,
 
 },
 {
-id: 8,
+id: 7,
 name: "Banane",
 price: 7,
-img: "https://m.media-amazon.com/images/I/61eeLzF-yyL._AC_UX342_.jpg"
+img: "https://m.media-amazon.com/images/I/61eeLzF-yyL._AC_UX342_.jpg",
+stock: 2,
 },
 {
-id: 9,
+id: 8,
 name: "Pesche",
 price: 6,
-img: "https://www.naturaatavola.it/sites/default/files/styles/image-full/public/nocepesche.naturaatavola.jpg?itok=cec-pVXJ"
+img: "https://www.naturaatavola.it/sites/default/files/styles/image-full/public/nocepesche.naturaatavola.jpg?itok=cec-pVXJ",
+stock: 3,
 },
 {
-    id: 10,
+    id: 9,
     name: "Pesce",
     price: 8,
-    img: "https://www.acquariodilivorno.it/data/srv_images/napoleone-1500x900.jpg"
+    img: "https://www.acquariodilivorno.it/data/srv_images/napoleone-1500x900.jpg",
+    stock: 200,
+},
+{
+id: 10,
+name: "Salmone",
+price: 70,
+img: "https://c.tenor.com/7xvdEql6Z28AAAAd/smirk-smirk-afk.gif",
+stock: 2,
 },
 {
 id: 11,
-name: "Salmone",
-price: 70,
-img: "https://c.tenor.com/7xvdEql6Z28AAAAd/smirk-smirk-afk.gif"
-},
-{
-id: 12,
 name: "Presonus Studio 24c",
 price: 240,
-img: "https://www.midimusic.it/wp-content/uploads/2022/01/presonus-studio_24c-front_big-800x800.jpg"
+img: "https://www.midimusic.it/wp-content/uploads/2022/01/presonus-studio_24c-front_big-800x800.jpg",
+stock: 25,
 },
 {
-    id: 13,
+    id: 12,
     name: "M-Audio BX8",
     price: 230,
-    img: "https://media.discopiu.com/img/2017/9/21/450384-large-m-audio-bx8-d3-coppia.jpg"
+    img: "https://media.discopiu.com/img/2017/9/21/450384-large-m-audio-bx8-d3-coppia.jpg",
+    stock: 86,
     },
     {
-    id: 14,
+    id: 13,
     name: "Macbook Air M1",
     price: 950,
-    img: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/refurb-macbook-air-silver-m1-202010?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1634145618000"
+    img: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/refurb-macbook-air-silver-m1-202010?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1634145618000",
+    stock: 350,
     },
     {
-        id: 15,
+        id: 14,
         name: "Toshiba Schifiu",
         price: 200,
-        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsXCp3QARwktMsHFDxZ5MU-aKzFuSMADtqvIOJiZrLj73L1GHpjKGCkjfLR1taO8b9x5E&usqp=CAU"
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsXCp3QARwktMsHFDxZ5MU-aKzFuSMADtqvIOJiZrLj73L1GHpjKGCkjfLR1taO8b9x5E&usqp=CAU",
+
     
     },
     {
-    id: 16,
+    id: 15,
     name: "Nintendo Switch Lite",
     price: 199,
     img: "https://mynintendostore.nintendo.it/media/catalog/product/cache/b4cb1f58b4f6fb6dd00a79cd43d6b8db/1/0/10002291-1-nswitchlitehwcoral-h2x1.jpg"
     },
     {
-    id: 17,
+    id: 16,
     name: "ProductHTML",
     price: 240,
-    img: "https://media-cdn.tripadvisor.com/media/photo-s/15/1f/32/58/grisouil.jpg"
+    img: "https://media-cdn.tripadvisor.com/media/photo-s/15/1f/32/58/grisouil.jpg",
+    stock: 2,
     },
     {
-        id: 18,
+        id: 17,
         name: "Acqua Panna",
         price: 24,
-        img: "https://www.bereacasa.it/wp-content/uploads/2021/04/acqua-panna-75cl.jpg"
+        img: "https://www.bereacasa.it/wp-content/uploads/2021/04/acqua-panna-75cl.jpg",
+        stock: 12,
         }
 ];
