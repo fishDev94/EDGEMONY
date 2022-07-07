ec95915ccd24da64a08dae90a2717d3d2a12cf6a
marco94gug <marco.guglielmino94@gmail.com>
2022-07-07 17:05:48 +0200

    feat: delete method added

--- 07-07/script.js
+++ 07-07/script.js
@@ -3,12 +3,12 @@ const getProductHTML = (product) => {
     
     
     return `
-    <li>
+    <li >
         <div class="element">
             <input type="checkbox">
             <span class="text-articles">${product}</span>
         </div>
-       
+        <div id="${shopList.products.indexOf(product)}" class="delete">x</div>
     </li>`;
   };
 
@@ -25,6 +25,10 @@ this._taskList.push(products)
 this.renderHTML();
 },
 
+set removeProduct(id) {
+    this._taskList.splice(this._taskList.indexOf(id), 1);
+    this.renderHTML();
+},
 
 renderHTML() {
     const $taskList = document.querySelector('.task-list');
@@ -44,8 +48,22 @@ shopList.products = $input.value;
 
 event.preventDefault();
 $input.value = '';
-console.log(shopList.products);
+// console.log(shopList.products);
+
+if (event.target.tagName === 'BUTTON') {
+    shopList.removeProduct = Number(event.target.id);
+
+}
 
 });
 
-console.log(shopList.products);
+const $deleteBtn = document.querySelector('.task-list');
+
+$deleteBtn.addEventListener('click', (event) => {
+
+    if (event.target.className === 'delete')
+    shopList.removeProduct = Number(event.target.id);
+    console.log(shopList.products);
+});
+
+
