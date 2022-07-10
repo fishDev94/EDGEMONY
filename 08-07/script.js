82e6fdb3e442e7192f24baeb1b8eb1604116b48a
marco94gug <marco.guglielmino94@gmail.com>
2022-07-10 15:27:33 +0200

    feat: added modal window

--- 08-07/script.js
+++ 08-07/script.js
@@ -1,6 +1,9 @@
 
 const $inputSender = document.querySelector('.search-form');
 const $input = $inputSender.querySelector('.form-control');
+const $resultList = document.querySelector('.results-list');
+const SITE_URL = "https://openlibrary.org";
+
 
 $inputSender.addEventListener('submit', function(e) {
     e.preventDefault();
@@ -16,7 +19,7 @@ $inputSender.addEventListener('submit', function(e) {
         })
         .then ((json) => {
 
-            const titles = json.docs.map((docs) => `<li data-key="${docs.key}">${docs.title}</li>`).join('');
+            const titles = json.docs.map((docs) => `<li>${docs.title}<button class="btn-view" id="${docs.key}">View</button></li> `).join('');
                const $list = document.querySelector('.results-ul');
                $list.innerHTML = `${titles}`; 
         })
@@ -24,13 +27,38 @@ $inputSender.addEventListener('submit', function(e) {
             console.log(error);
         })
 
+        if (e.target.classList === 'btn-view') {
+            console.log('hai premuto view')
+        }
+        console.log(e.target);
         $input.value = '';
     });
 
+const modalCointainerEl = document.querySelector('.modal-container');
+const btnClose = document.querySelector('.close');
+const pageContent = document.querySelector('.content');
 
+$resultList.addEventListener('click', (e) => {
+    
+    if (e.target.className === 'btn-view') {
+modalCointainerEl.classList.add('show');
+pageContent.innerHTML = `<object class="modal-content" type="text/html" data="${SITE_URL}${e.target.id}" >
+</object>`
+    
+}
+});
 
+modalCointainerEl.addEventListener('click', (e) => {
+    if (e.target.className === 'close') {
+        modalCointainerEl.classList.remove('show');
+    }
 
+    if (e.target.className === 'modal-container show') {
+        modalCointainerEl.classList.remove('show');  
+}
 
-
-
-        
+console.log(e.target)
+})
+// btnClose.addEventListener('click', () => {
+//     modalCointainerEl.classList.remove('show');
+// })
