556819f3eaad96af7e212268e46719e0eebf1252
marco94gug <marco.guglielmino94@gmail.com>
2022-07-12 11:58:57 +0200

    feat: ecommerce, filter on sidebar works

--- ecommerce/script.js
+++ ecommerce/script.js
@@ -40,12 +40,14 @@ const getProductHTML = (product) => {
     _page: 1, // pagina corrente
     _per_page: 6, // numero di risultati per pagina
     _id: 0,
+    _totalPages: 0,
+    // _totalPagesArray: [],
   
     get products() {
      
        const indexOfLastPost = (this._page * this._per_page); 
        const indexOfFirstPost = indexOfLastPost - this._per_page; 
-       
+       this._totalPages = Math.ceil(this._products.length/this._per_page)
 
   
       const paginatedProducts = this._products.slice(indexOfFirstPost, indexOfLastPost);
@@ -72,16 +74,30 @@ const getProductHTML = (product) => {
      */
     set page(newPage) {
      this._page = newPage;
-     this.renderHTML();   
+     this.renderHTML();  
+     
     },
   
     renderHTML() {   
         const productsEl = document.querySelector('.products');
         const productsHTML = this.products.map(getProductHTML).join('');
- 
+      
+
         productsEl.innerHTML = `
             <p>Pagina: ${this._page}</p>
             <ul class="products-ul">${productsHTML}</ul>`;
+    },
+
+    renderPagesButton() {
+      const $paging = document.querySelector('.buttons');
+      const btnArray = [];
+     for (let i=1; i<=shop._totalPages; i++) {
+       
+       const elBtn = `<button class="btn_pages" id="${i}"><p id="${i}">${i}</p></button>`;
+       btnArray.push(elBtn);
+       $paging.innerHTML = `${btnArray.join('')}`;
+     }
+
     }
   };
 
@@ -110,7 +126,37 @@ $productsContainer.addEventListener("click", (event) => {
 
 fetch('https://fakestoreapi.com/products')
             .then(res=>res.json())
-            .then(json=>shop.products = json);
+            .then(json => {
+              const $category = document.querySelector('.sidebar');
+              const $clearFilter = document.createElement('div');
+                  $clearFilter.classList.add('clear-filter');
+                  
+              $category.addEventListener("click", (event) => {
+                
+                if (event.target.tagName === "BUTTON") {
+                  console.log(event.target.id)
+                  shop.products = json.filter(element => element.category === event.target.id);
+
+                  $clearFilter.innerHTML = 'Clear filter';
+                  $category.append($clearFilter);
+                  shop.page = 1;
+                  shop.renderPagesButton();
+                }
+                
+              console.log(event.target);
+
+              if (event.target.className === "clear-filter") {
+                
+                event.target.remove();
+                shop.products = json
+                shop.renderPagesButton();
+                // console.log('mi hai cliccato');
+              }
+            });
+              shop.products = json
+              shop.renderPagesButton();
+            
+            })
 
 
 
@@ -124,13 +170,10 @@ fetch('https://fakestoreapi.com/products/categories')
               const category = json.map((e) => {
                 
                 console.log(e);
-              return `<li class="li-cat" id="${e}">${e}</li>`;
+              return `<button class="li-cat" id="${e}">${e}</button>`;
             }).join('');
             console.log(category);
             $categoriesList.innerHTML = `${category}`
-
-
-
           })
 
-            
+          
