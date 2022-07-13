f60902e8a331bcada297ec3e5a8661a1870d128a
marco94gug <marco.guglielmino94@gmail.com>
2022-07-13 11:30:51 +0200

    fix: localstorage bug when its empty

--- ecommerce/script.js
+++ ecommerce/script.js
@@ -1,26 +1,21 @@
 
-  
 const $buttonPages = document.querySelector('.buttons');
 const $productsContainer = document.querySelector('.products');
 const $cart = document.querySelector('.cart-list');
 const $cartIcon = document.querySelector('.cart');
 const $totalItems = document.querySelector('.items');
+const $category = document.querySelector('.sidebar');
+const $clearFilter = document.createElement('div');
 
-  // const values = Object.values(product.addons)
+$clearFilter.classList.add('clear-filter');
 
-  // let sum = 0;
-  // const fullOptionalPrice = values.forEach((val) => sum += val);
-  
-  // console.log(sum + product.price);
+localStorage.setItem('cart', JSON.stringify([]));
+localStorage.setItem('products', JSON.stringify([]));
 
-  
-  
 const getProductHTML = (product) => {
-    
     const {title, price, image, id} = product;
     function ivaCalc(price) {
       const res = (price/100) * 22
-      
       return res;
     }
     return `<li class="product-card"><img src="${image}"><p class="title_product">${title}</p>
@@ -30,10 +25,9 @@ const getProductHTML = (product) => {
   
   const getProductCartHTML = (product) => {
     
-    const {title, price, image, id} = product;
+    const {title, price, image} = product;
     function ivaCalc(price) {
       const res = (price/100) * 22
-      
       return res;
     }
     return `<li class="product-cart"><img src="${image}"><div class="textcard-cart"><p class="title_product">${title}</p>
@@ -44,44 +38,41 @@ const getProductHTML = (product) => {
   const shop = {
     name: "Edgemonics",
     _products: [],
-    _page: 1, // pagina corrente
-    _per_page: 6, // numero di risultati per pagina
+    _page: 1,
+    _per_page: 6,
     _id: 0,
     _totalPages: 0,
     _cart: [],
   
     get cart() {
-      // 
       $totalItems.innerText = `${this._cart.length} Items`
       localStorage.setItem('cart', JSON.stringify(this._cart));
       return this._cart;
-      
     },
 
     get products() {
       this._cart = JSON.parse(localStorage.getItem('cart'));
-       const indexOfLastPost = (this._page * this._per_page); 
-       const indexOfFirstPost = indexOfLastPost - this._per_page; 
-       this._totalPages = Math.ceil(this._products.length/this._per_page)
-      
-  
+      const indexOfLastPost = (this._page * this._per_page); 
+      const indexOfFirstPost = indexOfLastPost - this._per_page; 
+      this._totalPages = Math.ceil(this._products.length/this._per_page)
       const paginatedProducts = this._products.slice(indexOfFirstPost, indexOfLastPost);
+      this._products.map((product, index) => {product.id = index; return product})
       return paginatedProducts;
     },
-  
-    getPrice() {
-      
-      alert(`Super prezzo di oggi: ${this._products[this._id].price}€, Occhio! ne sono rimasti solamente ${this._products[this._id].stock}`)
-    }, 
 
     set idValue(id) {
       this._id = id - 1;
-      
     },
 
     set cartProduct(id) {
       this._cart.push(this._products[id]);
-      this._cart.map((product, index) => {product.id = index; return product})
+      console.log(id);
+      console.log(this._products);
+      this._cart.map((product, index) => {
+        product.id = index; 
+        
+        return product
+      })
       $totalItems.innerText = `${this._cart.length} Items`
     },
 
@@ -97,23 +88,16 @@ const getProductHTML = (product) => {
      */
     set page(newPage) {
      this._page = newPage;
-     this.renderHTML();  
-     
+     this.renderHTML(); 
     },
   
     set removeItemCart(id) {
-      this._cart.splice(id, 1);
-
-      // if (this._cart.)
-
-      // this._cart.remove(id);
+      this._cart.splice(id, 1)
     },
 
     renderHTML() {   
         const productsEl = document.querySelector('.products');
         const productsHTML = this.products.map(getProductHTML).join('');
-        
-        
         productsEl.innerHTML = `
             <p>Pagina: ${this._page}</p>
             <ul class="products-ul">${productsHTML}</ul>`;
@@ -122,25 +106,18 @@ const getProductHTML = (product) => {
     renderCartHTML() {
       const $cart = document.querySelector('.cart-list');
       const productsHTML = this.cart.map(getProductCartHTML).join('');
-console.log(this.cart);
       $cart.innerHTML = `<ul class="list-cart">${productsHTML}</ul>`
     },
 
     renderPagesButton() {
       const $paging = document.querySelector('.buttons');
       const btnArray = [];
-     for (let i=1; i<=shop._totalPages; i++) {
-       
+     for (let i=1; i<=shop._totalPages; i++) {      
        const elBtn = `<button class="btn_pages" id="${i}"><p id="${i}">${i}</p></button>`;
        btnArray.push(elBtn);
        $paging.innerHTML = `${btnArray.join('')}`;
-     };
-     
-
-    }
+     }}
   };
-
-
 
 $cartIcon.addEventListener("click", (event) => {
   $cart.classList.toggle('active');
@@ -151,100 +128,63 @@ $buttonPages.addEventListener("click", (event) => {
     const $button = event.target;
     const newPage = Number($button.id);
     shop.page = newPage;
-    
-  };
-  console.log(event.target);
-})
-
+  }
+});
 
 $productsContainer.addEventListener("click", (event) => {
   if (event.target.tagName === "BUTTON") {
-    // shop.idValue = event.target.id;
-    shop.cartProduct = event.target.id - 1;
-    console.log(shop.cart);
-    $cart.classList.remove('active');
+    shop.cartProduct = event.target.id;
     shop.renderCartHTML();
   }
-  
 });
 
 document.body.querySelector('.main-body').addEventListener("click", (event) => {
   if (event.target.className !== '.cart-list' || event.target.classList === '.nav-bar') {
     $cart.classList.remove('active');
   }
-})
-
+});
 
 $cart.addEventListener("click", (event) => {
-
   if (event.target.tagName === "BUTTON") {
     shop.removeItemCart = event.target.id;
     shop.renderCartHTML();
-    
-    console.log('mi stai cliccando');
-    console.log('il mio id:', event.target.id);
   }
 });
 
 fetch('https://fakestoreapi.com/products')
-            .then(res=>res.json())
-            .then(json => {
-              const $category = document.querySelector('.sidebar');
-              const $clearFilter = document.createElement('div');
-                  $clearFilter.classList.add('clear-filter');
-                  
-              $category.addEventListener("click", (event) => {
-                
-                if (event.target.tagName === "BUTTON") {
-                  console.log(event.target.id)
-                  shop.products = JSON.parse(localStorage.getItem('products')).filter(element => element.category === event.target.id);
-
-                  $clearFilter.innerHTML = 'Clear filter';
-                  $category.append($clearFilter);
-                  shop.page = 1;
-                  shop.renderPagesButton();
-                }
-                
-              console.log(event.target);
-
-              if (event.target.className === "clear-filter") {
-                
-                event.target.remove();
-                shop.products = json
-                shop.renderPagesButton();
-                // console.log('mi hai cliccato');
-              }
-            });
+  .then(res=>res.json())
+  .then(json => {
+    $category.addEventListener("click", (event) => {
+      if (event.target.tagName === "BUTTON") {
+      shop.products = JSON.parse(localStorage.getItem('products')).filter(element => element.category === event.target.id);
+      $clearFilter.innerHTML = 'Clear filter';
+      $category.append($clearFilter);
+      shop.page = 1;
+      shop.renderPagesButton();
+      }
+    
+      if (event.target.className === "clear-filter") {
+        event.target.remove();
+        shop.products = json;
+        shop.renderPagesButton();
+      }
+    }
+  );
             
-            if (!localStorage.getItem('products')) {
-              shop.products = json;
-            }
+      if (JSON.parse(localStorage.getItem('products')).length === 0) {
+        shop.products = json;
+      }
 
-              localStorage.setItem('products', JSON.stringify(json));
-              // shop.products = localStorage.getItem('products');
-
-              
-
-              
-            
-            })
-
-
-
-
-
+  localStorage.setItem('products', JSON.stringify(json));
+});
             
 fetch('https://fakestoreapi.com/products/categories')
             .then(res=>res.json())
             .then((json) => {
               const $categoriesList = document.querySelector('.categories-ul');
               const category = json.map((e) => {
-                
-                console.log(e);
-
               return `<button class="li-cat" id="${e}">${e}</button>`;
             }).join('');
-            console.log(category);
             $categoriesList.innerHTML = `${category}`
           })
 
