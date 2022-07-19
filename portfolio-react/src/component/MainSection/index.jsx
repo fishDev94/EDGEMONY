a89d5c98f9efdecaf2248ea25dfd4208b90ecebf
marco <marco.guglielmino94@gmail.com>
2022-07-19 23:55:56 +0200

    feat: added some style on buttons

--- portfolio-react/src/component/MainSection/index.jsx
+++ portfolio-react/src/component/MainSection/index.jsx
@@ -1,21 +1,21 @@
 import './index.css';
 import Button from '../Button';
 
-let text = 'empty';
-const Scrivi2 = () => {
-  text = document.getElementById('name').value;
-  document.getElementById('test').innerHTML = text
+let text = 'Il tuo nome';
+
+const scrivi = () => {
+  alert('Stai cliccando Knowledge');
+console.log(text)
+};
+
+const Scrivi2 = (e) => {
+  text = `${document.getElementById('name').value}`;
+  e.preventDefault();
+  document.getElementById('test').innerHTML = text;
   document.getElementById('name').value = '';
-// MainSection();
 };
 
 function MainSection() {
-  
-  const scrivi = () => {
-    alert('Stai cliccando Knowledge');
-  console.log(text)
-};
-  
 
   return (
     <div className="hero">
@@ -26,8 +26,10 @@ function MainSection() {
           <p className="content">Front-end Developer Computer enthusiast since childhood. I have various experience in the tech field, such as photo editing and sound design. Currently I am improving my javascript, specifically the reactJS framework at <span className="linkdesc">Edgemony</span>, to grow my knowledge and enhance my creativity.</p>
       </div>
       <Button action={scrivi} id="knowledge" text="Checkout my knowledge"/>
-      <input id="name" placeholder='scrivi il tuo nome'></input>
+      <form className="form-test">
+      <input id="name" placeholder='Scrivi il tuo nome'></input>
       <Button action={Scrivi2} id="Cisitu" text="Tu chi sei?"/>
+      </form>
       
       <h2 id="test">{text}</h2>
       
