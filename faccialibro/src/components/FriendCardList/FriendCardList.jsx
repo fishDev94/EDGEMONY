0531f1fc2367d84613d6a053c8ca6759905717be
marco <marco.guglielmino94@gmail.com>
2022-07-26 20:52:21 +0200

    feat: added clear filter button

--- faccialibro/src/components/FriendCardList/FriendCardList.jsx
+++ faccialibro/src/components/FriendCardList/FriendCardList.jsx
@@ -1,21 +1,33 @@
 import { useState, useEffect } from 'react'; 
 import FriendCard from '../FriendCard';
+import Button from '../Button';
 import { GET } from '../../utils/api';
 import './index.css';
 
-const FriendCardList = ({filterMsgFriends, searchValue}) => {
+const FriendCardList = ({filterMsgFriends}) => {
   const [friendList, setFriendList] = useState([]);
+  const [filterBtnVisible, setFilterBtnVisible] = useState(false);
 
   useEffect(() => {
     GET('friends').then(data => setFriendList(data));
   }, []);
 
+  
+ 
+
   return (
     <div className="FriendCardList">
       {
+       filterBtnVisible && <Button onClick={() => {filterMsgFriends('')
+
+      setFilterBtnVisible(false)}} textContent='Clear Filter' color='lightcoral'/>
+      }
+      {
         friendList.length
-          ? friendList.map(friend => <FriendCard key={friend.id} onHandleClick={() => {filterMsgFriends(friend.name)
-          console.log(searchValue) }} friendData={friend}/>)
+          ? friendList.map(friend => <FriendCard key={friend.id} onHandleClick={() => {
+            filterMsgFriends(friend.name)
+            setFilterBtnVisible(true)
+           }} friendData={friend}/>)
           : <p>Loading...</p>
       }
     </div>
