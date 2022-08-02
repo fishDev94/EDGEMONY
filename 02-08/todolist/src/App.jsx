f2c3e2f84ac40b22f9936609ceebcf4a8797f8cb
marco <marco.guglielmino94@gmail.com>
2022-08-02 22:20:49 +0200

    feat: 02-08 lesson with useReducer

--- 02-08/todolist/src/App.jsx
+++ 02-08/todolist/src/App.jsx
@@ -1,32 +1,60 @@
-import {useState, useReducer} from 'react';
+import {useReducer} from 'react';
+import React from 'react';
 import Button from './components/Button';
 import './App.css';
 import Input from './components/Input';
 import TodoList from './components/TodoList';
 
-function App() {
+let id = 0;
+const initData = {
+  listTask: [],
+};
+function reducer(state, action) {
+  let {listTask} = state
 
-const [list, setList] = useState([]);
-const [task, setTask] = useState('');
+switch (action.type) {
+  case ('insert') : { 
+    listTask = [...listTask, {...tryTask }]
+  }
+  case ('delete') : {
+    listTask = listTask.filter((obj) => obj.id -1 !== action.payload)
+  }
+}
+
+return {...state, listTask}
+}
+
+const tryTask = {};
+
+const App = () => {
+
+const [state, dispatch] = useReducer(reducer, initData);
 
 const onInputChange = (e) => {
-  setTask(e.target.value);
-
-}
+  tryTask.text = e.target.value
+  };
 
 const onHandleClick = (e) => {
   e.preventDefault();
-  setList([...list, task]);
-  setTask('');
+  tryTask['id'] = ++id;
+  dispatch({
+    type: 'insert', 
+  })
 }
 
+const onDeleteBtn = (id) => {
+  dispatch({
+    type: 'delete',
+    payload: id,
+  })
+}
   return (
     <div className="App">
       <form onSubmit={onHandleClick} className="form_todo" >
-        <Input onChange={onInputChange} inputValue={task}/>
+        <Input onChange={onInputChange} />
         <Button >Inserisci</Button>
       </form>
-      <TodoList setList={setList} data={list}/>
+      <TodoList data={state.listTask} dltBtn={() => onDeleteBtn}/>
     </div>
   );
 }
--- 02-08/todolist/src/components/TodoList/TodoList.jsx
+++ 02-08/todolist/src/components/TodoList/TodoList.jsx
@@ -1,18 +1,13 @@
 import './index.css';
 import TodoCard from '../TodoCard';
 
-const TodoList = ({data, setList}) => {
+const TodoList = ({data, dltBtn}) => {
 
-    const onHandleClick = (index) => {
-        // data.splice(index,1)
-        setList(data.filter((_, id) => id !== index) )
-        
-    }
 
     return (
         <ul className="TodoList">
             {
-                data.map((element, index, array) => <TodoCard id={index+1} data={array} onClick={onHandleClick} contentText={element} key={index}/>)
+                data.map((element) => <TodoCard contentText={element.text} onClick={dltBtn(element.id)} id={element.id} key={element.id}/>)
             }
         </ul>
 
