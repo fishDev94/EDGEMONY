cedf505d28f79f93419cf5895ca4095383261ef0
marco <marco.guglielmino94@gmail.com>
2022-09-01 13:52:10 +0200

    feat: first task completed - 01 Sept

--- 30-08/fancy-movies/src/components/MainSection/MainSection.jsx
+++ 30-08/fancy-movies/src/components/MainSection/MainSection.jsx
@@ -5,7 +5,12 @@ import CardList from '../CardList/CardList';
 import './index.css';
 
 export default function MainSection({ setMovieID, filmSection }){
-    const [movieLists, setMovieLists] = useState({});
+    const [movieLists, setMovieLists] = useState({
+      topRated: [{}],
+      popular: [{}],
+      upcoming: [{}],
+    });
+    const [filteredTopRated, setFilteredTopRated] = useState([]);
 
     useEffect(() => {
       GET('movie', 'popular', '&language=en-US&page=1')
@@ -16,19 +21,23 @@ export default function MainSection({ setMovieID, filmSection }){
         
       GET('movie', 'upcoming', '&language=en-US&page=1')
         .then(data => setMovieLists(prev => ({ ...prev, upcoming: data.results })));
+        
     }, []);
 
+    useEffect(() => {
+        setFilteredTopRated(movieLists.topRated.filter(movie => movie.vote_average >= 8.6))
+    }, [movieLists.topRated]);
 
     return (
         <div className="MainSection">
           <div className="MainSection_background--overlay" />
             <div className="MainSection_background" />
-            {movieLists.popular && <MainCard data={movieLists.popular[0]} setMovieID={setMovieID} filmSection={filmSection} className="MainCard_popular" />}
+            <MainCard data={movieLists.popular[0]} setMovieID={setMovieID} filmSection={filmSection} className="MainCard_popular" />
             <div className="MainSection_content">
             <h2>Top Rated:</h2>
-              {movieLists.topRated && <CardList data={movieLists.topRated} filmSection={filmSection} setMovieID={setMovieID} className="topRated"/>}
+              <CardList data={filteredTopRated} filmSection={filmSection} setMovieID={setMovieID} className="topRated"/>
               <h2>Upcoming:</h2>
-              {movieLists.upcoming && <CardList data={movieLists.upcoming} filmSection={filmSection} setMovieID={setMovieID} className="upcoming"/>}
+              <CardList data={movieLists.upcoming} filmSection={filmSection} setMovieID={setMovieID} className="upcoming"/>
             </div>
         </div>
     )
