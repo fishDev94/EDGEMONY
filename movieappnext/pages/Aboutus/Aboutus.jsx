0eafa4af43e0dcad42f1aee73011def2470bd0a0
marco <marco.guglielmino94@gmail.com>
2022-09-10 23:05:01 +0200

    test

---
+++ movieappnext/pages/Aboutus/Aboutus.jsx
@@ -0,0 +1,29 @@
+import styles from "./index.module.scss";
+import { useEffect } from "react";
+
+export default function Aboutus(props) {
+  const {
+    isModalVisibile,
+    movieID,
+    setMovieID,
+    setModalVisibility,
+    genreID,
+    typeofGenres,
+    pageNumber,
+    navBarPage,
+    setLinkActive,
+  } = props;
+
+  useEffect(() => {
+    setLinkActive("Aboutus");
+  }, []);
+
+  return (
+    <div className={styles.aboutus}>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero, eos
+        dolorem laborum consequuntur delectus temporibus?
+      </p>
+    </div>
+  );
+}
