be457150f3d33cdc014bb4fda1a46c24020aef0e
marco <marco.guglielmino94@gmail.com>
2022-09-12 15:42:24 +0200

    feat: added some style on scrollbar

--- 12-09/routingexcercise/src/components/NavBar/NavBar.jsx
+++ 12-09/routingexcercise/src/components/NavBar/NavBar.jsx
@@ -2,9 +2,27 @@ import { NavLink } from "react-router-dom";
 import styles from "./index.module.scss";
 
 export default function NavBar() {
+  const Links = [
+    { text: "Home", path: "/" },
+    { text: "About", path: "aboutus" },
+    { text: "Catalog", path: "catalog" },
+  ];
+
   return (
     <div className={styles.NavBar}>
-      <NavLink
+      {Links.map(({ text, path }) => (
+        <NavLink
+          to={path}
+          key={text}
+          className={({ isActive }) =>
+            isActive ? `${styles.link} ${styles.link_active}` : styles.link
+          }
+        >
+          {text}
+        </NavLink>
+      ))}
+
+      {/* <NavLink
         className={({ isActive }) =>
           isActive ? `${styles.link} ${styles.link_active}` : styles.link
         }
@@ -27,7 +45,7 @@ export default function NavBar() {
         to="catalog"
       >
         Catalog
-      </NavLink>
+      </NavLink> */}
     </div>
   );
 }
