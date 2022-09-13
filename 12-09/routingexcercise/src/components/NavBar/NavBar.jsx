11a0d80b8a77c41cfd57e27cfbcd0b61bd68efda
marco94gug <marco.guglielmino94@gmail.com>
2022-09-13 13:33:02 +0200

    chore: test

--- 12-09/routingexcercise/src/components/NavBar/NavBar.jsx
+++ 12-09/routingexcercise/src/components/NavBar/NavBar.jsx
@@ -4,6 +4,7 @@ import styles from "./index.module.scss";
 export default function NavBar() {
   return (
     <div className={styles.NavBar}>
+      <p>test</p>
       <NavLink
         className={({ isActive }) =>
           isActive ? `${styles.link} ${styles.link_active}` : styles.link
