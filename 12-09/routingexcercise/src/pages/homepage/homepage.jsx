93916ce5e7061726514671773a04b50f280292a2
marco <marco.guglielmino94@gmail.com>
2022-09-16 14:29:55 +0200

    feat: complete first task of 16sept excercise - pagination on homepage with useSearchParams

--- 12-09/routingexcercise/src/pages/homepage/homepage.jsx
+++ 12-09/routingexcercise/src/pages/homepage/homepage.jsx
@@ -3,10 +3,26 @@ import { background } from "../../constants/constants";
 // import { useFetch } from "../../utils/api/useFetch";
 // import { ENDPOINT } from "../../utils/api/endpoints";
 import CategoryList from "../../components/category-list/category-list";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useSearchParams } from "react-router-dom";
+import { useMemo, useState } from "react";
 
 function Homepage() {
   const data = useLoaderData();
+  const [pagePath, setPagePath] = useSearchParams({ page: 1 });
+  // const [page, setPage] = useState(1);
+  const page = pagePath?.get("page") ?? 1;
+
+  const perPage = 4;
+  const totalPages = Math.ceil(data?.categories?.length / perPage);
+  const catPaginated = useMemo(() => {
+    return data?.categories?.slice((page - 1) * perPage, page * perPage);
+  }, [data?.categories, page]);
+
+  const handleOnClick = (variation) => {
+    setPagePath((prev) => {
+      return { page: Number(prev.get("page")) + variation };
+    });
+  };
 
   if (!data?.categories?.length) {
     return "Not found";
@@ -20,10 +36,26 @@ function Homepage() {
       />
       <h1>What Shall We Eat Today?</h1>
       {data ? (
-        <CategoryList data={data?.categories} />
+        <CategoryList data={catPaginated} />
       ) : (
         "Errore durante il caricamento dei dati..."
       )}
+      <div className={styles.button_container}>
+        <button
+          onClick={handleOnClick.bind(this, -1)}
+          className={styles.button}
+          disabled={Number(pagePath?.get("page")) === 1}
+        >
+          Previous
+        </button>
+        <button
+          onClick={handleOnClick.bind(this, 1)}
+          className={styles.button}
+          disabled={Number(pagePath?.get("page")) === totalPages}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
