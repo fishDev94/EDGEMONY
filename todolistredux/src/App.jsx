0415b36ea1bbb865513ae3376cf8a54702e36763
marco <marco.guglielmino94@gmail.com>
2022-09-22 00:03:55 +0200

    feat: extra lesson 21 sept - Redux on Todo List

--- todolistredux/src/App.jsx
+++ todolistredux/src/App.jsx
@@ -1,15 +1,13 @@
 import styles from "./App.module.scss";
 import InputContainer from "./components/inputcontainer";
 import TaskList from "./components/tasklist";
-import { useSelector } from "react-redux";
 import NavBar from "./components/NavBar";
 
 function App() {
-  const store = useSelector((state) => state);
-
   return (
     <div className={styles.App}>
       <img
+        alt="background"
         className={styles.background}
         src="https://images.unsplash.com/photo-1569161031678-f49b4b9ca1c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
       />
--- todolistredux/src/components/inputcontainer/inputcontainer.jsx
+++ todolistredux/src/components/inputcontainer/inputcontainer.jsx
@@ -1,6 +1,6 @@
 import styles from "./index.module.scss";
 import { useDispatch } from "react-redux";
-import { useState, useRef } from "react";
+import { useState } from "react";
 
 export default function InputContainer() {
   const [inputValue, setInputValue] = useState("");
