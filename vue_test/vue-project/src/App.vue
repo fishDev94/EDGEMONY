5562f8f2ca9623de31185226b4790de2b5e5cf40
marco <marco.guglielmino94@gmail.com>
2022-10-19 23:30:10 +0200

    chore: init vue-cli app version 2

---
+++ vue_test/vue-project/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+<template>
+  <div id="app">
+    <h1>Home</h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+ #app {
+  text-align: center;
+  margin-top: 20px;
+  color: white;
+ }
+</style>
