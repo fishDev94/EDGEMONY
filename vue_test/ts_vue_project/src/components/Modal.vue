2214fa5d98f48393c35cff2cf2412b3b19a21385
marco94gug <marco.guglielmino94@gmail.com>
2022-10-29 17:09:51 +0200

    feat: added mob ver style for modal component

--- vue_test/ts_vue_project/src/components/Modal.vue
+++ vue_test/ts_vue_project/src/components/Modal.vue
@@ -50,15 +50,15 @@ export default {
         <button @click="handleSetFullScreen"></button>
       </div>
       <main>
-        <h2>{{ movieDetails.title }}</h2>
+        <h2>{{ movieDetails?.title }}</h2>
         <div class="genre_rating">
           <ul>
-            <li v-for="genre in movieDetails.genres">{{ genre.name }}</li>
+            <li v-for="genre in movieDetails?.genres">{{ genre.name }}</li>
           </ul>
-          <p>{{ movieDetails.vote_average.toFixed(1) }}/10 <span>★</span></p>
+          <p>{{ movieDetails?.vote_average?.toFixed(1) }}/10 <span>★</span></p>
         </div>
-        <p>
-          {{ movieDetails.overview }}
+        <p class="description">
+          {{ movieDetails?.overview }}
         </p>
       </main>
     </div>
@@ -88,7 +88,7 @@ export default {
 
   .modal_container {
     position: relative;
-    padding: 40px;
+    padding: 20px;
     border-radius: 10px;
     border: 1px solid rgb(70, 70, 70);
     background-color: rgb(42, 42, 42);
@@ -174,7 +174,7 @@ export default {
       margin-top: 30%;
       display: flex;
       flex-direction: column;
-      gap: 15px;
+      gap: 5px;
 
       h2 {
         font-weight: bold;
@@ -184,6 +184,8 @@ export default {
       .genre_rating {
         display: flex;
         justify-content: space-between;
+        flex-direction: column;
+        gap: 5px;
 
         ul {
           display: flex;
@@ -193,7 +195,9 @@ export default {
           margin: 0;
 
           li {
-            padding: 4px 15px;
+            padding: 4px 10px;
+            font-size: 0.8rem;
+            height: max-content;
             border-radius: 8px;
             color: rgb(48, 48, 48);
             font-weight: bold;
@@ -216,9 +220,172 @@ export default {
         }
       }
 
-      p {
+      .description {
         line-height: 28px;
         letter-spacing: 1.5px;
+        overflow-y: auto;
+        height: 180px;
+      }
+    }
+  }
+}
+
+@media screen and (min-width: 736px) {
+  .modal {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(0, 0, 0, 0.771);
+    backdrop-filter: blur(4px);
+
+    .overlay {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+    }
+
+    .modal_container {
+      position: relative;
+      padding: 40px;
+      border-radius: 10px;
+      border: 1px solid rgb(70, 70, 70);
+      background-color: rgb(42, 42, 42);
+      width: 75%;
+      height: 75%;
+      transition: all 0.25s;
+      overflow: hidden;
+
+      &.active {
+        width: 100%;
+        height: 100%;
+        border-radius: 0px;
+      }
+
+      .backdrop_path {
+        position: absolute;
+        top: 0;
+        left: 0;
+        height: 80%;
+        width: 100%;
+        object-fit: cover;
+        opacity: 0.4;
+      }
+
+      .overlay_backdrop {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: rgb(42, 42, 42);
+        background: -moz-linear-gradient(
+          0deg,
+          rgba(42, 42, 42, 1) 50%,
+          rgba(121, 9, 9, 0) 100%
+        );
+        background: -webkit-linear-gradient(
+          0deg,
+          rgba(42, 42, 42, 1) 50%,
+          rgba(121, 9, 9, 0) 100%
+        );
+        background: linear-gradient(
+          0deg,
+          rgba(42, 42, 42, 1) 50%,
+          rgba(121, 9, 9, 0) 100%
+        );
+        filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#2a2a2a",endColorstr="#790909",GradientType=1);
+      }
+
+      .button_container {
+        position: absolute;
+        top: 5px;
+        left: 5px;
+
+        button {
+          border: none;
+          border-radius: 100%;
+          height: 22px;
+          width: 22px;
+          color: rgba(0, 0, 0, 0);
+          font-weight: bold;
+          transform: scale(0.6);
+          cursor: pointer;
+
+          &:nth-child(1) {
+            background-color: rgb(209, 80, 80);
+          }
+
+          &:nth-child(2) {
+            background-color: rgb(99, 192, 99);
+            &:hover {
+              color: black;
+            }
+          }
+
+          &:hover {
+            color: rgba(0, 0, 0, 0.362);
+            transform: scale(0.7);
+          }
+        }
+      }
+      main {
+        margin-top: 30%;
+        display: flex;
+        flex-direction: column;
+        gap: 15px;
+
+        h2 {
+          font-weight: bold;
+          text-transform: uppercase;
+        }
+
+        .genre_rating {
+          display: flex;
+          justify-content: space-between;
+          flex-direction: row;
+
+          ul {
+            display: flex;
+            gap: 10px;
+            list-style: none;
+            padding: 0;
+            margin: 0;
+
+            li {
+              padding: 4px 15px;
+              border-radius: 8px;
+              color: rgb(48, 48, 48);
+              font-weight: bold;
+              background-color: rgb(212, 212, 212);
+            }
+          }
+
+          p {
+            font-weight: bold;
+
+            &:before {
+              content: "Rating: ";
+              font-weight: 100;
+              opacity: 0.6;
+            }
+
+            span {
+              color: gold;
+            }
+          }
+        }
+
+        p {
+          line-height: 28px;
+          letter-spacing: 1.5px;
+        }
       }
     }
   }
