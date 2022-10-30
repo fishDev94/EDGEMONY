6366ea8d309f1dbcdb1eb4a82757e39085df739f
marco94gug <marco.guglielmino94@gmail.com>
2022-10-30 13:25:18 +0100

    feat: working on about page

--- vue_test/ts_vue_project/src/views/AboutView.vue
+++ vue_test/ts_vue_project/src/views/AboutView.vue
@@ -1,18 +1,58 @@
 <script>
-export default {};
+import Polarix from "../assets/polarix.jpg";
+import MovieNextApp from "../assets/movienextapp.jpg";
+import Portfolio from "../assets/portfolio.jpg";
+
+export default {
+  data() {
+    return {
+      Polarix,
+      MovieNextApp,
+      Portfolio,
+    };
+  },
+
+  methods: {
+    onImgClick(url) {
+      window.open(url, "_blank");
+    },
+  },
+};
 </script>
 
 <template>
   <div class="about">
-    <h2>This is my first Vue APP</h2>
-    <p>
-      <a
-        href="https://github.com/marco94gug/EDGEMONY/tree/main/vue_test/ts_vue_project"
-        target="_blank"
-        >Here</a
-      >
-      You'll find the source code!
-    </p>
+    <section>
+      <h2>This is my first Vue APP</h2>
+      <p>
+        <a
+          href="https://github.com/marco94gug/EDGEMONY/tree/main/vue_test/ts_vue_project"
+          target="_blank"
+          >Here</a
+        >
+        You'll find the source code!
+      </p>
+    </section>
+    <section>
+      <h3>Here some project I made:</h3>
+      <div class="gallery">
+        <img
+          :src="Polarix"
+          alt="Polarix Next Project"
+          @click="onImgClick('https://project-north.vercel.app/')"
+        />
+        <img
+          :src="MovieNextApp"
+          alt="Movie Next App"
+          @click="onImgClick('https://movienextapp.vercel.app/')"
+        />
+        <img
+          :src="Portfolio"
+          alt="Portfolio"
+          @click="onImgClick('https://marcoguglielmino.com/')"
+        />
+      </div>
+    </section>
   </div>
 </template>
 
@@ -26,17 +66,46 @@ export default {};
     align-items: center;
     justify-content: center;
 
-    h2 {
-      font-weight: bold;
-    }
+    section {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
 
-    p {
-      a {
-        text-decoration: none;
-        color: rgb(138, 228, 220);
-        text-shadow: 0 0 4px rgba(255, 255, 255, 0.534);
+      h2 {
         font-weight: bold;
-        cursor: pointer;
+      }
+
+      p {
+        a {
+          text-decoration: none;
+          color: rgb(138, 228, 220);
+          text-shadow: 0 0 4px rgba(255, 255, 255, 0.534);
+          font-weight: bold;
+          cursor: pointer;
+        }
+      }
+
+      .gallery {
+        display: flex;
+        gap: 20px;
+        margin-top: 20px;
+
+        img {
+          position: relative;
+          width: 300px;
+          height: 250px;
+          object-fit: cover;
+          object-position: top;
+          box-shadow: 0 0 8px rgba(255, 255, 255, 0.226);
+          cursor: pointer;
+          transition: all 0.25s;
+
+          &:hover {
+            z-index: 1;
+            transform: scale(1.2);
+          }
+        }
       }
     }
   }
