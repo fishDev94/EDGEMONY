bd70fdae52b891158a5cd9d2fd6c7fa02be86c08
marco <marco.guglielmino94@gmail.com>
2022-11-03 19:41:47 +0100

    feat: testing Nuxt

---
+++ nuxt/testnuxtapp/pages/about.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  data() {
+    return {
+      name: "Hello World",
+    };
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>{{ name }}</h1>
+    <NuxtLink to="/">Home Page</NuxtLink>
+  </div>
+</template>
+
+<style scoped lang="scss">
+div {
+  h1 {
+    color: red;
+  }
+}
+</style>
---
+++ nuxt/testnuxtapp/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Home Page</h1>
+    <NuxtLink to="/about">About me</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
+
+<style scoped lang="scss"></style>
